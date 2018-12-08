e1a10f5653771bb2003dc5ba72d8a2b2d16aa2c5
OhChanHee <och5454@naver.com>
2018-12-08 11:53:26 +0900

    First

---
+++ Assets/script/Ccharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ccharacter : MonoBehaviour {
+    public int ch_id;
+    public string ch_name;
+    public int ch_shop;
+    public int ch_sh;
+    public int ch_speed;
+    public int ch_hp;
+    public int ch_spu;
+    public int ch_price;
+    public string ch_txt;
+    public bool ch_use=false;
+    public Sprite sprite;
+}
