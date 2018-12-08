d5f3f344a02222b24b00d8e3ab8538335a024e59
rjsdnqkr1 <rjsdnqkr1@dgsw.hs.kr>
2018-12-08 16:45:13 +0900

    UI업뎃

---
+++ Assets/script/Main/Money/Money_Update.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Money_Update : MonoBehaviour {
+    public Text money_text;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       money_text.text = Global.money.ToString() + "$";
+	}
+}
