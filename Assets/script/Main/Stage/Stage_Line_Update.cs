00c131cef44601ae3442a727a5b492e7e3b063e1
OhChanHee <och5454@naver.com>
2018-12-09 09:29:30 +0900

    lll

---
+++ Assets/script/Main/Stage/Stage_Line_Update.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Stage_Line_Update : MonoBehaviour {
+    public int stage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(stage <= Global.stage)
+        {
+            gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 255);
+        }
+	}
+}
---
+++ Assets/script/Start/Global.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour {
+    public static long money = 0; // 돈
+    public static int pg = 0; // 페이지 초기값
+    public static bool pg_use = false; // 페이지 사용 여부 
+    public static long exp = 0; // 경험치
+    public static int stage = 1; // 스테이지 잠금
+}
