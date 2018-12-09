97b853393ea6d894686b79b3f8bec35599dc2832
HyeonFe <choi030209@gmail.com>
2018-12-09 11:18:54 +0900

    soundOption
    아직 ui미완성 데스요

--- Assets/script/option/SoundOption.cs
+++ Assets/script/option/SoundOption.cs
@@ -1,20 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SoundOption : MonoBehaviour {
-    public AudioSour
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+public class SoundOption : MonoBehaviour
+{
+    public bool playEffect = true;
+    public bool playBGM = true;
 
-public class {
-    
+    public AudioSource BGM;
+    public AudioSource Effect;
+
+    public Text bgmWhat;
+    public Text effectWhat;
+
+
+    public void playSoundEffect(string snd) {
+       if (playEffect)
+            GameObject.Find(snd).GetComponent<AudioSource>().Play();
+    }
+
+    public void playSoundBGM(string snd) {
+        if (playBGM)
+            GameObject.Find(snd).GetComponent<AudioSource>().Play();
+    }
+
+    public void boolEffect() {
+        playEffect = !playEffect;
+
+    }
+
+    public void boolBGM() {
+        playBGM = !playBGM;
+
+    }
+
+
+  
+    void Update()
+    {
+        BGM.gameObject.SetActive(playBGM);
+        Effect.gameObject.SetActive(playEffect);
+
+        if (playEffect)
+            effectWhat.text = "효과음 : on";
+        else
+            effectWhat.text = "효과음 : off";
+
+        if (playBGM)
+            bgmWhat.text = "배경음 : on";
+        else
+            bgmWhat.text = "배경음 : off";
+    }
+
+
 }
