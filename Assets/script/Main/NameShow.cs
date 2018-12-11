abaafc25d1828a51342bd8c8f5d5d51e194dd3ab
rjsdnqkr1 <rjsdnqkr1@dgsw.hs.kr>
2018-12-11 10:45:46 +0900

    111

---
+++ Assets/script/Main/NameShow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NameShow : MonoBehaviour {
+    public Text name_text;
+    // Use this for initialization
+    void Start () {
+        name_text.text = InputProcessor.UserNick.ToString();
+    }
+	
+
+}
