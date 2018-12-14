f8a0d50ab7eebc4e7715ec2167a89776ddd2783f
rjsdnqkr1 <rjsdnqkr1@dgsw.hs.kr>
2018-12-14 09:57:20 +0900

    123

--- Assets/script/Main/Character/DataLoader.cs
+++ Assets/script/Main/Character/DataLoader.cs
@@ -20,21 +20,18 @@ public class DataLoader : MonoBehaviour {
     {
         if (Cchar_Mgr.ch[0].ch_use == true)
         {
-            //texts[0].text = "name:" + Cchar_Mgr.ch[2].ch_name;
             texts[1].text =Cchar_Mgr.ch[2].ch_speed.ToString();
             texts[2].text =Cchar_Mgr.ch[2].ch_hp.ToString();
             texts[3].text = Cchar_Mgr.ch[2].ch_txt;
         }
         else if (Cchar_Mgr.ch[1].ch_use == true)
         {
-            //texts[0].text = "name:" + Cchar_Mgr.ch[1].ch_name;
             texts[1].text =Cchar_Mgr.ch[1].ch_speed.ToString();
             texts[2].text =Cchar_Mgr.ch[1].ch_hp.ToString();
             texts[3].text =  Cchar_Mgr.ch[1].ch_txt;
         }
         else if (Cchar_Mgr.ch[2].ch_use == true)
         {
-            //texts[0].text = "name:" + Cchar_Mgr.ch[0].ch_name;
             texts[1].text =  Cchar_Mgr.ch[0].ch_speed.ToString();
             texts[2].text = Cchar_Mgr.ch[0].ch_hp.ToString();
             texts[3].text = Cchar_Mgr.ch[0].ch_txt;
