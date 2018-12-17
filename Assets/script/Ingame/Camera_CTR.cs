1f1d43557b45a8ea038565345cbe11662c1d4112
rjsdnqkr1 <rjsdnqkr1@dgsw.hs.kr>
2018-12-17 17:14:22 +0900

    123

---
+++ Assets/script/Ingame/Camera_CTR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_CTR : MonoBehaviour {
+    public GameObject A;
+    Transform AT;
+    void Start()
+    {
+        AT = A.transform;
+    }
+    void Update()
+    {
+        Limit();
+        transform.position = Vector3.Lerp(transform.position, new Vector3 (AT.position.x,0,AT.position.z), 2f * Time.deltaTime);
+        transform.Translate(0, 0, -10); //카메라를 원래 z축으로 이동
+    }
+    void Limit()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.x = Mathf.Clamp(transform.position.x, 1, 412);
+
+        transform.position = new Vector3(pos.x, transform.position.y, transform.position.z);
+
+    }
+}
