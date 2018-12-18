c204902f1869d73c4aebe49c03216a09b66b3d55
OhChanHee <och5454@naver.com>
2018-12-18 09:52:41 +0900

    ddd

---
+++ Assets/script/Ingame/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+    public GameObject particle;
+	// Use this for initialization
+	void Start () {
+        Invoke("Create", 3);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Create()
+    {
+        Instantiate(particle, transform.position, Quaternion.identity);
+        Destroy(particle, 3);
+    }
+}
