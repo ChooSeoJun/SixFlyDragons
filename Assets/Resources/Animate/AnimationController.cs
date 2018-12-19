dd17123ef0727fcd7c16e0f4d39853a1d78bbd58
추서준 <chuseojun@naver.com>
2018-12-19 11:39:50 +0900

    애니메이

---
+++ Assets/Resources/Animate/AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        if( anim == null )
+            Debug.Log("anim == null");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float f = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Key Space");
+            anim.SetBool("Running", true);
+            anim.SetBool("Run", true);
+            anim.SetTrigger("Jump");
+        }
+        else if( Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetBool("Running", false);
+            anim.SetBool("Run", true);
+        }
+        
+
+
+    }
+}
---
+++ Assets/Resources/Animate/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movePower = 1f;
+
+    Animator animator;
+    Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+        
+        animator = gameObject.GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            animator.SetBool("isMovinag", false);
+            animator.SetInteger("Direction", 0);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            animator.SetInteger("Direction", -1); // 좌측
+            animator.SetBool("isMovinag", true);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            animator.SetInteger("Direction", 1); // 우측
+            animator.SetBool("isMovinag", true);
+        }
+	}
+    void FixedUpdate ()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+
+    //      transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+
+      //      transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+}
